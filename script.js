c4e3c4f6350da5958cf858a49e74963a50fd59de
Fabius Gasber <fabiusgasber@gmail.com>
2023-12-20 15:38:40 +0100

    Add variables for password validation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputs = Array.from(document.getElementsByTagName("input"));
 const spans = Array.from(document.getElementsByTagName("span"));
+const pwd1 = document.getElementById("pwd");
+const pwd2 = document.getElementById("pwd2");
 
 inputs.forEach((input) => {
     input.addEventListener("blur", (e) => {
