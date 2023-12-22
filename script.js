a7fff05e22d4e483f1604877abbfaf79da377bbd
Fabius Gasber <fabiusgasber@gmail.com>
2023-12-22 12:52:08 +0100

    Add functions to show and remove required password pattern

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const inputs = Array.from(document.getElementsByTagName("input"));
 const spans = Array.from(document.getElementsByTagName("span"));
+const pwdRules = Array.from(document.querySelectorAll("span ~ p"));
 const pwd1 = document.getElementById("pwd");
 const pwd2 = document.getElementById("pwd2");
+
+pwd1.addEventListener("focus", showRules);
+pwd1.addEventListener("blur", removeRules);
+
+function showRules() {
+    pwdRules.forEach((rule) => {
+        rule.style.display = "block";
+    });
+}
+
+function removeRules(){
+    pwdRules.forEach((rule) => {
+        rule.style.display = "none";
+    });
+}
 
 inputs.forEach((input) => {
     input.addEventListener("blur", (e) => {
